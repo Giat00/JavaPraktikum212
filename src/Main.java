6018fc4e31fa79a085e3b0f95a327e57307280e0
Giat00 <adigiat00@gmail.com>
2022-06-05 19:36:30 +0800

    menambahkan helper

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+
+import helper.MyConnection;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,8 @@
  * @author GIAT
  */
 public class Main {
-    
+       public static void main(String[] args){
+           MyConnection my = new MyConnection();
+           my.getConnection();
+       }
 }
